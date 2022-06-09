6d5e485dbce37c397471b6e9dd24f838c1dbd238
Flammrock <flammrockgame@gmail.com>
2022-06-09 08:49:46 +0200

    fix auth configuration

--- src/components/AuthenticatorGateway.tsx
+++ src/components/AuthenticatorGateway.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 import {
   IonButton,
@@ -8,30 +8,58 @@ import {
 } from '@ionic/react';
 
 //import { API } from 'aws-amplify';
+import Auth from '@aws-amplify/auth';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 
 import './AuthenticatorGateWay.css';
 
+interface IUser {
+  name: string;
+  email: string;
+}
 
 const AuthenticatorGateWay: React.FC<{ children: React.ReactElement, path?: string }> = ({ children, path }) => {
 
+  let [user, setUser] = useState<IUser|boolean|null>(null);
+  useEffect(() => {
+    let updateUser = async () => {
+      try {
+        let user = await Auth.currentAuthenticatedUser();
+        setUser(user);
+      } catch {
+        setUser(false);
+      }
+    };
+    updateUser();
+  }, []);
+
+  if (user==null) return <div className="auth-content"></div>;
+
   if (typeof path === 'string') {
+    if (typeof user === 'boolean' && !user) {
+      return (
+        <div className="auth-content">
+          <IonList>
+            <IonItem>
+              <IonLabel>
+                <strong>You do not have enough permissions to access this content.</strong>
+              </IonLabel>
+            </IonItem>
+            <IonItem>
+              <Link to="/auth"><IonButton>Click here to login</IonButton></Link>
+            </IonItem>
+          </IonList>
+        </div>
+      );
+    } else {
     return (
       <div className="auth-content">
-        <IonList>
-          <IonItem>
-            <IonLabel>
-              <strong>You do not have enough permissions to access this content.</strong>
-            </IonLabel>
-          </IonItem>
-          <IonItem>
-            <Link to="/auth"><IonButton>Click here to login</IonButton></Link>
-          </IonItem>
-        </IonList>
+        {children}
       </div>
     );
+    }
   } else {
     return (
       <div className="auth-content">
